e2ef20c94d431caf2194a0326e5b2eb6ed1be849
Mohamad <lemirmohammad.shehab@lau.edu>
2017-11-01 18:19:43 +0200

    Implemented AsyncImageListener, button to download image, and handling the display of progress bar

--- app/src/main/java/mchehab/com/asynctaskdemo/MainActivity.java
+++ app/src/main/java/mchehab/com/asynctaskdemo/MainActivity.java
@@ -1,21 +1,19 @@
 package mchehab.com.asynctaskdemo;
 
-import android.os.AsyncTask;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-
-public class MainActivity extends AppCompatActivity implements AsyncListener{
+public class MainActivity extends AppCompatActivity implements AsyncListener, AsyncImageListener{
 
     private TextView textView;
+    private ImageView imageView;
+    private ProgressBar progressBar;
     private final String URL = "http://validate.jsontest.com/?json=%7B%22key%22:%22value%22%7D";
 
     @Override
@@ -24,15 +22,29 @@ public class MainActivity extends AppCompatActivity implements AsyncListener{
         setContentView(R.layout.activity_main);
 
         textView = findViewById(R.id.textView);
+        imageView = findViewById(R.id.imageView);
+        progressBar = findViewById(R.id.progressBar);
+
         Button button = findViewById(R.id.button);
+        Button buttonImage = findViewById(R.id.buttonImage);
 
         button.setOnClickListener(e -> {
             new GetJSON(this).execute(URL);
+        });
+        buttonImage.setOnClickListener(e -> {
+            progressBar.setVisibility(View.VISIBLE);
+            new AsyncImageDownloader(this).execute("https://www.w3schools.com/bootstrap/paris.jpg");
         });
     }
 
     @Override
     public void getResult(String result) {
         textView.setText(result);
+    }
+
+    @Override
+    public void getBitmap(Bitmap bitmap) {
+        progressBar.setVisibility(View.GONE);
+        imageView.setImageBitmap(bitmap);
     }
 }
