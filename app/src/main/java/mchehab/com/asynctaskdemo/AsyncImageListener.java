196d36bec29f63bc9872449996318bd7839fc156
Mohamad <lemirmohammad.shehab@lau.edu>
2017-11-01 18:09:58 +0200

    Created an interface for image AsyncTask

---
+++ app/src/main/java/mchehab/com/asynctaskdemo/AsyncImageListener.java
@@ -0,0 +1,11 @@
+package mchehab.com.asynctaskdemo;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by muhammadchehab on 11/1/17.
+ */
+
+public interface AsyncImageListener {
+    public void getBitmap(Bitmap bitmap);
+}
