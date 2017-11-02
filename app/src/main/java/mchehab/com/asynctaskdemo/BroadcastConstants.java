974fa58ee0b433ac03ae89932384640839b5b65f
Mohamad <lemirmohammad.shehab@lau.edu>
2017-11-02 23:43:43 +0200

    Added files in order to store constant String literals

---
+++ app/src/main/java/mchehab/com/asynctaskdemo/BroadcastConstants.java
@@ -0,0 +1,12 @@
+package mchehab.com.asynctaskdemo;
+
+/**
+ * Created by muhammadchehab on 11/2/17.
+ */
+
+public class BroadcastConstants {
+
+    public static final String JSON = "json";
+    public static final String IMAGE = "image";
+
+}
---
+++ app/src/main/java/mchehab/com/asynctaskdemo/SaveInstanceConstants.java
@@ -0,0 +1,11 @@
+package mchehab.com.asynctaskdemo;
+
+/**
+ * Created by muhammadchehab on 11/2/17.
+ */
+
+public class SaveInstanceConstants {
+    public static final String MAIN_ACTIVITY_IMAGE = "image";
+    public static final String MAIN_ACTIVITY_TEXT = "text";
+    public static final String MAIN_ACTIVITY_ASYNC_JSON = "asyncImageDownloader";
+}
