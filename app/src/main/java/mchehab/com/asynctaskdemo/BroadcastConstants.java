9095528e75bc4853fc0a5d4bc6bf940afec24caf
Mohamad <lemirmohammad.shehab@lau.edu>
2017-11-06 18:37:31 +0200

    Added constant for posting JSON

--- app/src/main/java/mchehab/com/asynctaskdemo/BroadcastConstants.java
+++ app/src/main/java/mchehab/com/asynctaskdemo/BroadcastConstants.java
@@ -8,5 +8,6 @@ public class BroadcastConstants {
 
     public static final String JSON = "json";
     public static final String IMAGE = "image";
+    public static final String JSON_POST = "json post";
 
 }
